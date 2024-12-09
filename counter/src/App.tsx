0a50af5052e2579c8467e7709d65d9aac2235761
Jigar Lodaya <jigar.lodaya12@gmail.com>
2024-12-09 22:50:39 +0530

    counter based timer added

--- counter/src/App.tsx
+++ counter/src/App.tsx
@@ -1,7 +1,69 @@
+import { useRef, useState } from "react";
 import "./App.css";
 
 function App() {
-  return <>Hello</>;
+  const [counter, setCounter] = useState(0);
+  const [isCounterRunning, setIsCounterRunning] = useState(false);
+  const intervalRef = useRef<ReturnType<typeof setInterval> | null>(null);
+
+  const startCounter = () => {
+    if (!isCounterRunning) {
+      setIsCounterRunning(true);
+      intervalRef.current = setInterval(() => {
+        setCounter((prev) => prev + 1);
+      }, 1000);
+    }
+  };
+  const stopCounter = () => {
+    if (isCounterRunning) {
+      setIsCounterRunning(false);
+      if (intervalRef.current) clearInterval(intervalRef.current);
+    }
+  };
+  const resetCounter = () => {
+    stopCounter();
+    setCounter(0);
+  };
+  const resumeCounter = () => {
+    startCounter();
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+      <h1 className="text-2xl">Counter</h1>
+      <br />
+      {counter}
+      <div className="">
+        <button
+          className="p-3 px-4 rounded-xl bg-green-500 m-2 text-white"
+          onClick={startCounter}
+          disabled={isCounterRunning}
+        >
+          Start
+        </button>
+        <button
+          className="p-3 px-4 rounded-xl bg-red-500 m-2 text-white"
+          onClick={stopCounter}
+          disabled={!isCounterRunning}
+        >
+          Pause
+        </button>
+        <button
+          className="p-3 px-4 rounded-xl bg-blue-500 m-2 text-white"
+          onClick={resumeCounter}
+          disabled={!isCounterRunning}
+        >
+          Resume
+        </button>
+        <button
+          className="p-3 px-4 rounded-xl bg-gray-500 m-2 text-white"
+          onClick={resetCounter}
+        >
+          Reset
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
